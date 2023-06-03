a43bcf7f6a42f200fd33921d383968ec71be1e6e
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-06-03 22:18:12 +0300

    added basic project setup aand routing

---
+++ cartrader/components/Car/SearchBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div
+      class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
+    >
+      <input
+        type="text"
+        class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+        :class="
+          requestError ? 'bg-gradient-to-r from-red-300 to-white-500' : ''
+        "
+        placeholder="Search by city..."
+        v-model="city"
+      />
+      <button class="bg-sky-500 px-10 text-white" @click="searchCar">
+        Search
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+      requestError: false,
+    };
+  },
+  methods: {
+    searchCar() {
+      if (this.city.length > 0) {
+        this.requestError = false;
+        navigateTo(`/city/${this.city}/car`);
+      } else {
+        this.requestError = true;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ cartrader/pages/car/[name]-[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <CarDetailHero></CarDetailHero>
+      <CarDetailAttributes></CarDetailAttributes>
+      <CarDetailContact></CarDetailContact>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "../../components/NavBar.vue";
+export default {
+  components: { NavBar },
+};
+</script>
+
+<style scoped></style>
